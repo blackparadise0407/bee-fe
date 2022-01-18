697bfc43cb99281ce1b2d33c2bd21c7e474e8353
blackparadise0407 <blackparadise0407@gmail.com>
2022-01-19 00:03:10 +0700

    Implement Navigation component

---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,25 @@
+import {} from 'react-icons/ai'
+import { HiArrowRight, HiArrowLeft } from 'react-icons/hi'
+import { RiSearch2Line } from 'react-icons/ri'
+import { Input } from '..'
+
+type NavigationProps = {}
+
+export default function Navigation({}: NavigationProps) {
+    return (
+        <div className="flex items-center">
+            <div className="flex space-x-5">
+                <HiArrowLeft className="text-xl text-gray-600 hover:text-gray-500 transition-colors cursor-pointer" />
+                <HiArrowRight className="text-xl text-gray-600 hover:text-gray-500 transition-colors cursor-pointer" />
+            </div>
+            <Input
+                className="ml-10"
+                icon={<RiSearch2Line />}
+                fullWidth
+                inputProps={{
+                    placeholder: 'Search for artist, songs and...',
+                }}
+            />
+        </div>
+    )
+}
